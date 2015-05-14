9be21bc932423b21b2626fe2794286f4f715bfa7
Ang Hao Yang <yan9za1@gmail.com>
2015-05-15 01:37:07 +0800

    Add listening port condition for Heroku.

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ queue.on('connected', function() {
 });
 
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
 
   var host = server.address().address;
   var port = server.address().port;
